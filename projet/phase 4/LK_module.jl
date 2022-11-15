b74f86c6abddcc6431c314dfedc12dd555baaf25
LuisRojoGonzalez <113364801+LuisRojoGonzalez@users.noreply.github.com>
2022-11-15 12:39:24 -0500

    Update LK_module.jl
    Updates the LK function

--- projet/phase 4/LK_module.jl
+++ projet/phase 4/LK_module.jl
@@ -6,180 +6,159 @@ using .Comp_module
 export lin_kernighan
 
 """
-Takes as parameters a graph, an algorithm for MST, a root , iteration limit, time limit, step size, adaptive
-returns an Tour
+Takes as parameters a graph, an algorithm for MST, a root (or nothing in whose case the root is randomly selected at each iteration),
+iteration limit, time limit, step size as a vector of two numbers to bound the random number generator, an adaptive as a boolean to indicate whether
+the step size update uses a fraction (0.3) of the previous result. It returns the best solution found as a graph, its cost, whether it is a tour
+and the elapsed time it took.
 """
-function lin_kernighan(g::Graph, algorithm::Function, root::Node, max_iterations::Int64, max_time::Int64, step::Float64, adaptive::Bool) where T
+function lin_kernighan(g::Graph, algorithm::Function, root::Union{Node, nothing}, max_iterations::Int64, max_time::Int64, step::Vector{Float64,Float64}, adaptive::Bool) where T
 
-    #####################################
-    ##### here starts the algorithm #####
-    #####################################
-
-    # tracks the improvement over the cost function
-    incumbent = 0 # starting lower bound
-
-    # initialize the counter for the iterations
-    iter = 1
-
+    # call the required libraries
+    using Random
+    Random.seed!(seed) # set the seed for the random number generator
+    
     # clocks the time
     starting_time = time()
-    elapsed_time = time()-starting_time
-
+    elapsed_time = time() - starting_time
+    
+    # creates starting objects to iteratively modify them
     one_tree = Graph{T}()
-    graph_degree= zeros(Int, nb_nodes(g))
+    tree_comp = zeros(Int, nb_nodes(g))
+    tree_cost = 0
+    dual_cost = 0
 
-    og_weights = Dict{Edge{T}, Float64}()
+    # creates a dictionary that indicates the degree of each node in the 1-tree
+    dict_pi = Dict{Node, Float64}()
+    for n in nodes(graph)
+        dict_pi[n] = 0.0
+    end
+    
+    # creates a dictionary that represents the difference of degree with a tour
+    graph_degree, graph_degree_prev = Dict{Node, Float64}(), Dict{Node, Float64}()
+    for n in nodes(graph)
+        graph_degree[n] = 0.0
+        graph_degree_prev[n] = 0.0
+    end
+
+    # create a dictionary with the original weights
+    og_weights = Dict{Edge, Float64}()
     for e in edges(g)
         og_weights[e] = weight(e)
     end
 
-    dict_pi = Dict{Node{T}, Float64}()
-    for n in nodes(g)
-        dict_pi[n] = 0.0
-    end
+    # tracks the improvement over the cost function
+    incumbent = -Inf # starting lower bound
+
+    # initial step size
+    step_size = rand(step[1]:0.1:step[2])
+
+    # initialize the counter for the iterations
+    iter = 1
+
+    # the tour indicador is set to be false
+    is_tour = false
+    
     while iter <= max_iterations && elapsed_time <= max_time
 
-        # print out the progress so far
         println("-----------------------------------------------------------------------------------------------------------")
         println("--- Iteration: $(iter) | Elapsed time (s): $(round(elapsed_time; digits = 4)) | Incumbent: $(incumbent) ---")
         println("-----------------------------------------------------------------------------------------------------------")
 
-        ##################################################################
-        ################## STEP 2: Construct the 1-tree ##################
-        ##################################################################
-
         # changes the weights in the graph
-        for edge in edges(g)
+        for edge in edges(graph)
             # get the ends of the edge
-            n1, n2 = ends(edge)           
-            set_weight!(edge, max(0,og_weights[edge] - dict_pi[n1] - dict_pi[n2]))
+            n1, n2 = ends(edge)
+
+            set_weight!(edge, og_weights[edge] + dict_pi[n1] + dict_pi[n2])
         end
-       
-        # constructs the 1-tree
- 
-        one_tree, tree_comp = one_tree(Graph{T}("", nodes(g), copy(edges(g))), algorithm, root)
-  
-        #################################################################
-        ################## STEP 3: Update the cost ######################
-        #################################################################
+        
+        # construct the 1-tree and the degree of the nodes
+        if !isnothing(root)
+            global one_tree, tree_comp = one_tree(Graph{T}("", nodes(g), copy(edges(g))), algorithm, root)
+        else
+            global one_tree, tree_comp = one_tree(Graph{T}("", nodes(g), copy(edges(g))), algorithm, nodes(graph)[rand(1:nb_nodes(graph))])
+        end
+        
         # compute the total cost of the 1-tree
-        tree_cost = sum(weight.(edges(one_tree)))
+        global tree_cost = sum(weight.(edges(one_tree)))
 
-        # update the cost
-        dual_cost = tree_cost - 2*sum(values(dict_pi))
+        # compute the lagrangian
+        global dual_cost = tree_cost - 2*sum(values(dict_pi))
 
-        #################################################################
-        ############# STEP 4: Get the current incumbent #################
-        #################################################################
-        incumbent = max(incumbent, dual_cost)
-   
-        #################################################################
-        ################ STEP 5: Get the graph degree ###################
-        #################################################################
-        # if the way is said not to be adaptive then
-        # both graph_degree are the same to save lines below
-        # otherwise, save the previous graph_degree and then 
-        # update it
+        # update the incumbent
+        global incumbent = max(incumbent, dual_cost)
 
-        graph_degree = [degree(tree_comp, n) - 2 for n in nodes(g)]
-        # if !adaptive
-        #     graph_degree = [degree(TreeDegree, n) - 2 for n in nodes(g)]
-        #     graph_degree_prev =copy(graph_degree)
-        # else
-        #     if iter == 1
-        #         graph_degree = [degree(TreeDegree, n) - 2 for n in nodes(g)]
-        #         graph_degree_prev = copy(graph_degree)
-        #     else
-        #         graph_degree_prev = copy(graph_degree)
-        #         graph_degree = [degree(TreeDegree, n) - 2 for n in nodes(g)]
-        #     end
-        # end
+        # getting the difference with a degree of 2
+        if !adaptive
+            for n in nodes(graph)
+                graph_degree[n] = degree(tree_comp, n) - 2
+                graph_degree_prev[n] = graph_degree_prev[n]
+            end
+        else
+            if iter == 1
+                for n in nodes(graph)
+                    global graph_degree[n] = degree(tree_comp, n) - 2
+                    global graph_degree_prev[n] = graph_degree_prev[n]
+                end
+            else
+                for n in nodes(graph)
+                    global graph_degree_prev[n] = graph_degree[n]
+                    global graph_degree[n] = degree(tree_comp, n) - 2
+                end
+            end
+        end
 
-        #################################################################
-        ################## STEP 6: Check for a tour #####################
-        #################################################################
-        if all(graph_degree .== 0)
+        # check for a tour
+        if all(collect(values(graph_degree)) .== 0)
+            
             # here we should get the original weights in the tree
             for edge in edges(one_tree)
-                # get the ends of the edge
-                n1, n2 = ends(edge)
-                set_weight!(get_edge(one_tree, n1, n2), weight(get_edge(g, n1, n2)))
+
+                # replace the weights
+                set_weight!(edge, og_weights[edge])
             end
 
             # update the 1-tree cost
-            tree_cost = sum(weight.(edges(one_tree)))
+            global tree_cost = sum(weight.(edges(one_tree)))
             println("---------------------------------------------")
             println("----- A Hamiltonian tour has been found -----")
-            println("---- The tour found has cost $(TreeCost) ----")
+            println("---- The tour found has cost $(tree_cost) ----")
             println("---------------------------------------------")
             # indicator to check whether the solution is a tour
-            is_tour = false
+            global is_tour = true
 
             # updates the time
-            elapsed_time = time()-starting_time
+            global elapsed_time = time() - starting_time
 
             return one_tree, tree_cost, is_tour, elapsed_time
             break
         end
 
-        #################################################################
-        ################ STEP 7: Update the step size ###################
-        #################################################################
-        # compute the norm
-        norm_degree = sqrt(sum(values(degrees(tree_comp)) .^ 2))
-  
         # update the step size
-        @show step
-        @show incumbent
-        @show dual_cost
-        # if incumbent - dual_cost > 0
-        #     step = (step) * (incumbent - dual_cost ) / norm_degree 
-        # end
-      
-        #################################################################
-        ################ STEP 8: Update reduced costs ###################
-        #################################################################
-        # if not adaptive graph_degree and graph_degree_prev are equal
-    
-  
-        for n in nodes(g)
-            dict_pi[n] = dict_pi[n] - step * (degree(tree_comp, n) - 2)
+        global step_size = rand(step[1]:0.1:step[2]) / iter
+
+        # Update reduced costs
+        for n in nodes(graph)
+            dict_pi[n] = dict_pi[n] + step_size * (0.7*graph_degree[n] + 0.3*graph_degree_prev[n])
         end
-        
-        #################################################################
-        ################ STEP 8: Update reduced costs ###################
-        #################################################################
+
         # increase the counter
-        iter = iter + 1
+        global iter = iter + 1
 
         # updates the time
-        elapsed_time = time()- starting_time
+        global elapsed_time = time() - starting_time
     end
 
-    println("---------------------------------------------")
-    println("--- A Hamiltonian tour could not be found ---")
-    println("---------------------------------------------")
-    # indicator to check whether the solution is a tour
-    is_tour = false
-
-    # here we should get the original weights in the tree
-    for edge in edges(one_tree)
-        # get the ends of the edge
-        n1, n2 = ends(edge)
-
-        set_weight!(get_edge(one_tree, n1, n2), weight(get_edge(g, n1, n2)))
-        
+    #### now it actually print out according to the progress and not when going out of the loop
+    if !is_tour
+        println("----------------------------------------------------")
+        println("------ A Hamiltonian tour has not been found -------")
+        println("-------- Best lower bound costs $(incumbent) -------")
+        println("----------------------------------------------------")
     end
-    # update the 1-tree cost
-    tree_cost = sum(weight.(edges(one_tree)))
-
-    # updates the time
-    elapsed_time = time() - starting_time
     
     return one_tree, tree_cost, is_tour, elapsed_time
-
 end
-
 
 """
 Take a graph, an algorithm and a root, and build a one-tree with this root. Returns the tree (a graph struct) and a component structure describing the tree.
@@ -215,6 +194,4 @@ function one_tree(g::Graph{T}, algorithm::Function, root::Node{T}) where T
     degrees(c)[root] = 2
 
     return tree, c
-end
-
 end
