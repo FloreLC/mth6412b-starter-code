00f79afa6814c85464fe7a5f061b7cb45b02477d
flore <florecaye220@gmail.com>
2022-11-21 19:46:23 -0500

    ready to submit

--- projet/phase 4/main.jl
+++ projet/phase 4/main.jl
@@ -6,10 +6,12 @@ using Random
 using DataFrames, CSV
 
 const SEED = 42
+
+files_override_loop = [ARGS[1]]
 instances_folder = joinpath("..", "..", "instances", "stsp")
 for (root, dirs, files) in walkdir(joinpath("..", "..", "instances", "stsp"))
 
-    for f in files[[14]]
+    for f in files_override_loop ## remplacer par files pour lancer le main sur toutes les instances
         instance = chop(f, tail=4)
         Random.seed!(SEED) # set the seed for the random number generator
 
@@ -58,26 +60,26 @@ for (root, dirs, files) in walkdir(joinpath("..", "..", "instances", "stsp"))
         CSV.write(joinpath("results", "$(instance)_rsl.csv"), results_rsl)
 
         # ### ----- Test Loop for LK ------- ##
-        # println("---- Solving instance $(instance) with LK----")
-        # for param in eachrow(grid_lk)
-        #     graph = build_graph(file)
-        #     # apply the algorithm to find the tour
+        println("---- Solving instance $(instance) with LK----")
+        for param in eachrow(grid_lk)
+            graph = build_graph(file)
+            # apply the algorithm to find the tour
 
-        #     root = nodes(graph)[1]
-        #     if param[:root_strat] == 1
-        #         root = nodes(graph)[1]
-        #     elseif param[:root_strat] == 2
-        #         root = nodes(graph)[rand(1:nb_nodes(graph))]
-        #     elseif param[:root_strat] == 3
-        #         root = nodes(graph)[max_degre(graph)]
-        #     end
-        #     solution, cost_lk, tour, time_lk = lin_kernighan(graph, param[:algo], root, 1000, 60, [param[:step], param[:step] + 1], param[:adapt])
+            root = nodes(graph)[1]
+            if param[:root_strat] == 1
+                root = nodes(graph)[1]
+            elseif param[:root_strat] == 2
+                root = nodes(graph)[rand(1:nb_nodes(graph))]
+            elseif param[:root_strat] == 3
+                root = nodes(graph)[max_degre(graph)]
+            end
+            solution, cost_lk, tour, time_lk = lin_kernighan(graph, param[:algo], root, 1000, 60, [param[:step], param[:step] + 1], param[:adapt])
 
-        #     # create mst with different techniques and add the times to the results object
-        #     push!(results_lk, [param[:root_strat], param[:algo], param[:step], param[:adapt], tour, cost_lk, time_lk])
-        # end
-        # ## write results
-        # CSV.write(joinpath("results", "$(instance)_lk.csv"), results_lk)
+            # create mst with different techniques and add the times to the results object
+            push!(results_lk, [param[:root_strat], param[:algo], param[:step], param[:adapt], tour, cost_lk, time_lk])
+        end
+        ## write results
+        CSV.write(joinpath("results", "$(instance)_lk.csv"), results_lk)
 
     end
 end
