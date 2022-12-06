5922bb8f20429507e730056658ab270b294428f6
LuisRojoGonzalez <113364801+LuisRojoGonzalez@users.noreply.github.com>
2022-12-05 19:57:03 -0500

    Adds the report

--- projet/phase 5/rapport5.jl
+++ projet/phase 5/rapport5.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.12
+# v0.19.16
 
 using Markdown
 using InteractiveUtils
@@ -44,7 +44,8 @@ md"""
 ## Choisir les parametres
 ### Algorithmes
 
-Comme le graphe representant les images ont un sommet relie a tous les autres sommets par une arete de poids 0, alors tout arbre couvrant de poids minimal est compose uniquement de ce sommet initial et de toutes les aretes nulles. L'algorithme RSL se base sur un MST pour fournir une tournee. Ainsi RSL n'est pas pertinent pour la resolution de nos problemes. \\
+Comme le graphe representant les images ont un sommet relie a tous les autres sommets par une arete de poids 0, alors tout arbre couvrant de poids minimal est compose uniquement de ce sommet initial et de toutes les aretes nulles. L'algorithme RSL se base sur un MST pour fournir une tournee. Ainsi RSL n'est pas pertinent pour la resolution de nos problemes.
+
 Nous allons donc nous concentrer sur les valeurs des parametres de notre implementation de l'algorithme HK.
 ### HK: Construire une tournee a partir d'un 1-arbre
 
@@ -62,142 +63,86 @@ Notre strategie est donc la suivante:
 Les parametres sur lesquels nous intervenons sont les suivant:
 - Algorithme pour produire un MST (Prim ou Kruskal)
 - Pas adaptatif ou non (True ou False)
-- Time limit (par defaut 2 minutes)
+- Time limit (par defaut cinq minutes)
 - iteration limit (par defaut 10 000)
 - Valeur de pas (un interval de valeurs)
 
 De nos experiences lors des precedentes phases du projet, nous avons retenu que Kruskal etait systematiquement plus rapide que Prim. Nous nous contentons donc de cet algorithme. Pour les memes raisons, nous ne considerons que des situation ou le pas est adaptatif. 
 #### Impact du pas
-Tout d'abord, considerons pour une meme image, avec une limite de temps de 2 minutes, differentes valeur de pas:
-1. [0.5, 1.0], lenght = 125534.58
+Tout d'abord, considerons pour une meme image, avec une limite de temps de cinq minutes et un pas initialle dans l'interval [0.5, 30.0]. L'algorithme montre les résultats suivants, où le nombre de nœuds de degré 2 est indiqué.
+"""
+
+# ╔═╡ 2f54747c-06a1-43d9-bbe1-f6905cf04759
+md"""
+1. Image: abstract-light-painting.png, nœuds en tournée = 600 (Tourne).
 """
 
 # ╔═╡ 0d617a15-cab5-4222-a4f2-941e992e0eda
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[0.5, 1.0]_true_kruskal_pre.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/abstract-light-painting.png")
 
 # ╔═╡ d8de9d7e-ef39-4a8a-a3f7-08f1474142bb
 md"""
-2. [0.5, 50.0], lenght =  125534.58
+2. Image: alaska-railroad, nœuds en tournée = 597.
 """
 
 # ╔═╡ 9bf33fae-e451-4669-9a49-6d76d236eb06
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[0.5, 50.0]_true_kruskal_pre.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/alaska-railroad.png")
 
 # ╔═╡ 7b8fc419-7a0f-485d-a6bb-a95b74f7a2d8
 md"""
-3. [0.5, 100.0], lenght = 125534.58
+3. Image: blue-hour-paris, nœuds en tournée = 593.
 """
 
-
-# ╔═╡ b66df03a-9b7d-4f5a-92b4-4f21c4ddbb21
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[0.5, 100.0]_true_kruskal_pre.png")
+# ╔═╡ 8baaa0df-d36d-44f9-9692-7eef782105d3
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/blue-hour-paris.png")
 
 # ╔═╡ 6cd11def-aa96-4e36-8a24-6269402b2f20
 md"""
-4. [1.0, 10.0], lenght = 125534.58
+4. Image: lower-kananaskis-lake, nœuds en tournée = 591.
 """
 
 # ╔═╡ a021d457-40f9-4194-a95c-24f396f6b51c
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[1.0, 10.0]_true_kruskal_pre.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/lower-kananaskis-lake.png")
 
 # ╔═╡ afefed9c-b03e-4ff7-b1d1-36e330df5131
 md"""
-5. [20.0, 30.0], lenght = 125534.58
+5. Image: marlet2-radio-board, nœuds en tournée = 591.
 """
 
 # ╔═╡ cd0c751a-4387-4aed-9092-50fa2af0b1ff
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[20.0, 30.0]_true_kruskal_pre.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/marlet2-radio-board.png")
 
 # ╔═╡ 56ff91e1-6068-4c78-9849-0e633e6f38c8
 md"""
-6. [20.0, 30.0], lenght = 125534.58, sans pas adaptatif
+6. Image: nikos-cat, nœuds en tournée = 595.
 """
 
 # ╔═╡ eeb84de7-3c60-4091-bc3d-384d3826f0fe
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[20.0, 30.0]_false_kruskal_pre_120.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/nikos-cat.png")
 
 # ╔═╡ cabf0d44-d6fd-4664-b448-5d8d6349de48
 md"""
-7. [30.0, 50.0], lenght = 125534.58
+7. Image: pizza-food-wallpaper, nœuds en tournée = 595.
 """
 
 # ╔═╡ ccc73325-3a0a-4ca1-9515-ef6ce3d0d62f
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/blue-hour-paris_lk_[30.0, 50.0]_true_kruskal_pre.png")
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/pizza-food-wallpaper.png")
 
-# ╔═╡ c268d287-97af-4190-930d-66c7295863e7
+# ╔═╡ 15c93e37-a88c-4014-9798-c67b02c4ea08
 md"""
-Si la longueur des tournees obtenues est toujours la meme, on peut tout de meme remarquer que les images 5,6 et 7 se rapprochent plus de l'image originale que les 4 premieres.
-En particulier il semble que le pas adaptatif, s'il permet d'avoir une meilleure idee de la structure de l'image, n'aggrege pas beaucoup de colonnes voisines.
+8. Image: the-enchanted-garden, nœuds en tournée = 600 (Tourne).
 """
 
-# ╔═╡ abe66ed7-b3b4-4abf-ad9e-425ace8d46fb
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/original/blue-hour-paris.png")
+# ╔═╡ 439ded92-76f3-427b-89d8-04148fe20e83
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/the-enchanted-garden.png")
 
-# ╔═╡ 8fa5ffb2-6552-4571-aece-4c46820a43cd
+# ╔═╡ a610894b-2cd3-488a-b461-50e5e0fd4cef
 md"""
-#### Impact du temps
-Tout parametre egal par ailleurs, nous avons voulu etablir si la duree d'execution permettait de produire des solutions plus satisfaisantes. Il nous semble (et nous arrivons a la meme conclusions sur plusieurs instances) que la solution trouvee en 2 minutes n'est pas si differente de celles trouvees en 5, 10 ou 15 min.
-1. 2min, lenght = 118111.05
+9. Image: tokyo-skytree-aerial, nœuds en tournée = 600 (Tourne).
 """
 
-# ╔═╡ 3dc5ed22-6172-4d6e-98ad-4dd0f390c4f0
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/lower-kananaskis-lake_lk_[20.0, 30.0]_true_kruskal_pre.png")
-
-# ╔═╡ e98f04c2-9a89-482c-b9fd-9b3a948b5b3c
-md"""
-2. 10 min, lenght = 118111.05
-"""
-
-# ╔═╡ 6e80add1-6eb6-4586-a80f-c1d1d06153e3
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/lower-kananaskis-lake_lk_[20.0, 30.0]_true_kruskal_pre_600.png")
-
-# ╔═╡ f01b3cb5-c846-473a-988f-c287291715cb
-md"""
-3. 20 min, lenght = 118111.05
-"""
-
-# ╔═╡ fca334e1-06f9-41ee-bc21-41a2ee848d9b
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/lower-kananaskis-lake_lk_[20.0, 30.0]_true_kruskal_pre_1200.png")
-
-# ╔═╡ b2d7c3e0-8b56-4637-9ea5-a6751b973fbf
-md"""
-Originale:
-"""
-
-# ╔═╡ e953b5cd-b313-4c29-b908-bcb646e81b9f
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/original/lower-kananaskis-lake.png")
-
-# ╔═╡ 8c793f92-e4ef-46e6-abc9-788a8f88c619
-md"""
-L'amelioration ne nous semble pas valoir l'invesitssement en ressource.
-## Pas adaptable
-Enfin, nous comparons nos resultats avec et sans le pas adaptable sur quelques instances avec une limite de temps de 2min, un pas de [1.0, 2.0] et l'algorithme de Kruskal pour les MST:
-- Sans le pas adaptable
-"""
-
-# ╔═╡ 41691877-4cb2-47ba-8787-47dd675d5d39
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/marlet2-radio-board_lk_[1.0, 2.0]_false_kruskal_pre_120.png")
-
-# ╔═╡ a63c2255-3466-421b-9783-040fa0be2bac
-md"""
-- Avec le pas adaptable
-"""
-
-# ╔═╡ 3464e994-1557-4280-979a-2da40ac2ac14
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/reconstructed/marlet2-radio-board_lk_[1.0, 2.0]_true_kruskal_pre_120.png")
-
-# ╔═╡ 7ad9cc87-a71a-4dd1-bce0-de45320de000
-md"""
-- Image originale:
-"""
-
-# ╔═╡ 44ff3522-ac51-4605-a0a2-7d79874e9516
-Wow("/Users/flore/Desktop/Cours/MTH6412B/Projet/mth6412b-starter-code/projet/phase 5/shredder-julia/images/original/marlet2-radio-board.png")
-
-# ╔═╡ 806e7f6f-a9b8-466c-9ead-13090682369f
-md"""
-On observe que bine que le pas adaptable donne des "clusters" de colonnes plus larges, les coutours des elements principaux restent bien visible avec ou sans le pas adaptable.
-"""
+# ╔═╡ cfd27cbf-e5bc-4c60-96a2-f26dc10383db
+Wow("/Users/luisrojo/Library/CloudStorage/OneDrive-usach.cl/PhD/Courses_Polymtl/OR Algorithms/Laboratory/mth6412b-starter-code/projet/phase 5/shredder-julia/images/tokyo-skytree-aerial.png")
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -212,8 +157,9 @@ PlutoUI = "~0.7.48"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.7.3"
+julia_version = "1.8.2"
 manifest_format = "2.0"
+project_hash = "97be6e027681c6ecfa37671630e179d506eb1167"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -223,6 +169,7 @@ version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -239,6 +186,7 @@ version = "0.11.4"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "0.5.2+0"
 
 [[deps.Dates]]
 deps = ["Printf"]
@@ -247,6 +195,7 @@ uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
 [[deps.Downloads]]
 deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
 
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
@@ -288,10 +237,12 @@ version = "0.21.3"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -300,6 +251,7 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -323,19 +275,23 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.0+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2022.2.1"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.20+0"
 
 [[deps.Parsers]]
 deps = ["Dates", "SnoopPrecompile"]
@@ -346,6 +302,7 @@ version = "2.5.1"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.8.0"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
@@ -372,6 +329,7 @@ version = "1.2.2"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -395,10 +353,12 @@ uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.1"
 
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
@@ -424,28 +384,33 @@ uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.12+3"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.1.1+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
 # ╟─a89f8fdc-6f65-11ed-1c24-9b5108ae25c6
 # ╟─c28eaa50-b91c-4a29-8810-c559f756d19a
+# ╟─2f54747c-06a1-43d9-bbe1-f6905cf04759
 # ╟─0d617a15-cab5-4222-a4f2-941e992e0eda
 # ╟─d8de9d7e-ef39-4a8a-a3f7-08f1474142bb
 # ╟─9bf33fae-e451-4669-9a49-6d76d236eb06
 # ╟─7b8fc419-7a0f-485d-a6bb-a95b74f7a2d8
-# ╟─b66df03a-9b7d-4f5a-92b4-4f21c4ddbb21
+# ╟─8baaa0df-d36d-44f9-9692-7eef782105d3
 # ╟─6cd11def-aa96-4e36-8a24-6269402b2f20
 # ╟─a021d457-40f9-4194-a95c-24f396f6b51c
 # ╟─afefed9c-b03e-4ff7-b1d1-36e330df5131
@@ -454,22 +419,9 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╟─eeb84de7-3c60-4091-bc3d-384d3826f0fe
 # ╟─cabf0d44-d6fd-4664-b448-5d8d6349de48
 # ╟─ccc73325-3a0a-4ca1-9515-ef6ce3d0d62f
-# ╟─c268d287-97af-4190-930d-66c7295863e7
-# ╟─abe66ed7-b3b4-4abf-ad9e-425ace8d46fb
-# ╟─8fa5ffb2-6552-4571-aece-4c46820a43cd
-# ╟─3dc5ed22-6172-4d6e-98ad-4dd0f390c4f0
-# ╟─e98f04c2-9a89-482c-b9fd-9b3a948b5b3c
-# ╟─6e80add1-6eb6-4586-a80f-c1d1d06153e3
-# ╟─f01b3cb5-c846-473a-988f-c287291715cb
-# ╟─fca334e1-06f9-41ee-bc21-41a2ee848d9b
-# ╟─b2d7c3e0-8b56-4637-9ea5-a6751b973fbf
-# ╟─e953b5cd-b313-4c29-b908-bcb646e81b9f
-# ╟─8c793f92-e4ef-46e6-abc9-788a8f88c619
-# ╟─41691877-4cb2-47ba-8787-47dd675d5d39
-# ╟─a63c2255-3466-421b-9783-040fa0be2bac
-# ╟─3464e994-1557-4280-979a-2da40ac2ac14
-# ╟─7ad9cc87-a71a-4dd1-bce0-de45320de000
-# ╟─44ff3522-ac51-4605-a0a2-7d79874e9516
-# ╟─806e7f6f-a9b8-466c-9ead-13090682369f
+# ╟─15c93e37-a88c-4014-9798-c67b02c4ea08
+# ╟─439ded92-76f3-427b-89d8-04148fe20e83
+# ╟─a610894b-2cd3-488a-b461-50e5e0fd4cef
+# ╟─cfd27cbf-e5bc-4c60-96a2-f26dc10383db
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
